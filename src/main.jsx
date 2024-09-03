ae0ef8aa2f0314574a23891a33b8439672f0f697
Hardy121 <merh0783@gmail.com>
2024-09-03 17:42:44 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import App from './App.jsx'
 import './index.css'
 import { QueryClient, QueryClientProvider, } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Product from './components/product.jsx'
+import Product from './components/Product.jsx'
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
